e1311eff3565b10f000f096f08ac73f5efe3f820
ivaylo.a <ivaylo.a@sbtech.com>
2018-02-05 14:00:41 +0200

    added - mockResponse.js

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -2,6 +2,7 @@ import { shallow , mount,configure} from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import React from 'react';
 import App from '../App';
+import mockData from './mockResponse';
 import HeaderNavigation from '../components/HeaderNavigation';
 
 configure({ adapter: new Adapter() });
@@ -15,17 +16,21 @@ describe('<App />', () => {
     const instance = appComponent.instance();
     expect(instance).toBeDefined();
   });
-
-  // it('<App /> should loadPhotos call filterDublicatePhotos', () => {
+ 
+  // it('<App /> [loadPhotos] works fine', () => {
   //   const instance = appComponent.instance();
-  //   instance.loadPhotos();
-  //   expect(instance.filterDublicatePhotos).toBeCalled();
+  //   const jsonMockData = JSON.parse(mockData);
+  //   instance.loadPhotos(mockData);
+    
+  //   expect(instance.state.photosArray.length).toEqual(jsonMockData.length)
+  //   expect(instance.state.isFetching).toEqual(false)
   // });
 
   // it('<HeaderNavigationComponent> simulates click Search', () => {
   //   const instance = headerNavigationComponent.instance();
-  //   var mockEvent = jest.fn();
+  //   instance.handleSubmit = jest.fn(() => mockData);
   //   headerNavigationComponent.find('#search-btn').simulate('click');
-  //   expect(instance.handleSubmit(mockEvent)).toBeCalled();
+  //   expect(instance.handleSubmit).toBeCalled();
   // });
+});
 });
---
+++ src/__tests__/mockResponse.js
@@ -0,0 +1,77 @@
+var mockData = {
+    "title": "Recent Uploads tagged wildlife",
+    "link": "https://www.flickr.com/photos/tags/wildlife/",
+    "description": "",
+    "modified": "2016-07-28T17:21:48Z",
+    "generator": "https://www.flickr.com/",
+    "items": [
+   {
+        "title": "Great crested grebe",
+        "link": "https://www.flickr.com/photos/143458341@N05/28578725986/",
+        "media": {"m":"https://farm8.staticflickr.com/7541/28578725986_197e48c7ed_m.jpg"},
+        "date_taken": "2016-07-18T20:42:39-08:00",
+        "description": " <p><a href=\"https://www.flickr.com/people/143458341@N05/\">David and Lisa Mowbray<\/a> posted a photo:<\/p> <p><a href=\"https://www.flickr.com/photos/143458341@N05/28578725986/\" title=\"Great crested grebe\"><img src=\"https://farm8.staticflickr.com/7541/28578725986_197e48c7ed_m.jpg\" width=\"157\" height=\"240\" alt=\"Great crested grebe\" /><\/a><\/p> ",
+        "published": "2016-07-28T17:06:01Z",
+        "author": "nobody@flickr.com (David and Lisa Mowbray)",
+        "author_id": "143458341@N05",
+        "tags": "waterbird grebe greatcrestedgrebe loch lake wildlife nature"
+   },
+   {
+        "title": "Raw Power",
+        "link": "https://www.flickr.com/photos/99745957@N00/27994313744/",
+        "media": {"m":"https://farm9.staticflickr.com/8619/27994313744_0392ed9960_m.jpg"},
+        "date_taken": "2008-01-08T07:57:15-08:00",
+        "description": " <p><a href=\"https://www.flickr.com/people/99745957@N00/\">rollermt<\/a> posted a photo:<\/p> <p><a href=\"https://www.flickr.com/photos/99745957@N00/27994313744/\" title=\"Raw Power\"><img src=\"https://farm9.staticflickr.com/8619/27994313744_0392ed9960_m.jpg\" width=\"240\" height=\"161\" alt=\"Raw Power\" /><\/a><\/p> <p>Lion, Masai Mara, Kenya. via 500px <a href=\"http://ift.tt/2ae6rtI\" rel=\"nofollow\">ift.tt/2ae6rtI<\/a><\/p>",
+        "published": "2016-07-28T17:08:58Z",
+        "author": "nobody@flickr.com (rollermt)",
+        "author_id": "99745957@N00",
+        "tags": "ifttt 500px nature animals animal wildlife power strength plains africa kenya lions lion safari masai mara honeymoon kicheche masaimara"
+   },
+   {
+        "title": "DSC_1170",
+        "link": "https://www.flickr.com/photos/114186746@N06/28532654841/",
+        "media": {"m":"https://farm9.staticflickr.com/8156/28532654841_cf7c34714b_m.jpg"},
+        "date_taken": "2016-06-18T16:36:06-08:00",
+        "description": " <p><a href=\"https://www.flickr.com/people/114186746@N06/\">gsas777<\/a> posted a photo:<\/p> <p><a href=\"https://www.flickr.com/photos/114186746@N06/28532654841/\" title=\"DSC_1170\"><img src=\"https://farm9.staticflickr.com/8156/28532654841_cf7c34714b_m.jpg\" width=\"240\" height=\"160\" alt=\"DSC_1170\" /><\/a><\/p> <p>Coot Chicks<\/p>",
+        "published": "2016-07-28T17:08:53Z",
+        "author": "nobody@flickr.com (gsas777)",
+        "author_id": "114186746@N06",
+        "tags": "coots chicks ducklings birds avian wetland wildlife water ponds babies"
+   },
+   {
+        "title": "DSC_1153",
+        "link": "https://www.flickr.com/photos/114186746@N06/27994953573/",
+        "media": {"m":"https://farm9.staticflickr.com/8837/27994953573_02bf6857c9_m.jpg"},
+        "date_taken": "2016-06-18T16:31:20-08:00",
+        "description": " <p><a href=\"https://www.flickr.com/people/114186746@N06/\">gsas777<\/a> posted a photo:<\/p> <p><a href=\"https://www.flickr.com/photos/114186746@N06/27994953573/\" title=\"DSC_1153\"><img src=\"https://farm9.staticflickr.com/8837/27994953573_02bf6857c9_m.jpg\" width=\"240\" height=\"160\" alt=\"DSC_1153\" /><\/a><\/p> <p>Coot Chicks<\/p>",
+        "published": "2016-07-28T17:08:52Z",
+        "author": "nobody@flickr.com (gsas777)",
+        "author_id": "114186746@N06",
+        "tags": "coots chicks ducklings birds avian wetland wildlife water ponds babies"
+   },
+   {
+        "title": "I look like dinner",
+        "link": "https://www.flickr.com/photos/143458341@N05/27994304544/",
+        "media": {"m":"https://farm9.staticflickr.com/8845/27994304544_e135b9cfba_m.jpg"},
+        "date_taken": "2016-07-02T17:08:58-08:00",
+        "description": " <p><a href=\"https://www.flickr.com/people/143458341@N05/\">David and Lisa Mowbray<\/a> posted a photo:<\/p> <p><a href=\"https://www.flickr.com/photos/143458341@N05/27994304544/\" title=\"I look like dinner\"><img src=\"https://farm9.staticflickr.com/8845/27994304544_e135b9cfba_m.jpg\" width=\"240\" height=\"100\" alt=\"I look like dinner\" /><\/a><\/p> ",
+        "published": "2016-07-28T17:09:04Z",
+        "author": "nobody@flickr.com (David and Lisa Mowbray)",
+        "author_id": "143458341@N05",
+        "tags": "buzzard hawk bird wildlife nature"
+   },
+   {
+        "title": "noch eine ...",
+        "link": "https://www.flickr.com/photos/noyvo/27994899773/",
+        "media": {"m":"https://farm8.staticflickr.com/7533/27994899773_bcefe12fd4_m.jpg"},
+        "date_taken": "2016-07-23T12:53:33-08:00",
+        "description": " <p><a href=\"https://www.flickr.com/people/noyvo/\">Norbert Helbig<\/a> posted a photo:<\/p> <p><a href=\"https://www.flickr.com/photos/noyvo/27994899773/\" title=\"noch eine ...\"><img src=\"https://farm8.staticflickr.com/7533/27994899773_bcefe12fd4_m.jpg\" width=\"240\" height=\"160\" alt=\"noch eine ...\" /><\/a><\/p> <p>... Mistbiene.<br /> <br /> Das Pflanzen der beiden Sommerflieder-Büsche in diesem Frühjahr hat sich schon mal gelohnt :o)))<\/p>",
+        "published": "2016-07-28T17:07:56Z",
+        "author": "nobody@flickr.com (Norbert Helbig)",
+        "author_id": "137201158@N06",
+        "tags": "nikon d5200 natur nature wildlife outdoor insekten insekt tiere fliege schwebfliege blüten"
+   }
+    ]
+}
+
+export default mockData;
